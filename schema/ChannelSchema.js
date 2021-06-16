3d6ee523e605da90a1a3e667eb2e48bb76777345
Alexander Mason <alex@alexmason.me>
2021-06-15 21:30:09 -0400

    Removed graphql and references

--- schema/ChannelSchema.js
+++
@@ -1,7 +0,0 @@
-module.exports = `
-type Channel {
-  id: ID!
-  name: String!
-  description: String!
-}
-`;
--- schema/OrganizationSchema.js
+++
@@ -1,7 +0,0 @@
-module.exports = `
-type Organization {
-  id: ID!
-  name: String!
-  description: String!
-}
-`;
